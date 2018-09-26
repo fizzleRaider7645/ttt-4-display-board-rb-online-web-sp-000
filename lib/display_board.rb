2b4698aaa85ac917377ab67ccccb03addd20aaad
Douglas Uretsky <douglasuretsky@gmail.com>
2018-09-26 18:27:07 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,13 +1,11 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 
-def display_board(board = Array.new(9) {:X})
-  def display_board
+def display_board(board = Array.new(9) {" "})
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
-end
 end
 
