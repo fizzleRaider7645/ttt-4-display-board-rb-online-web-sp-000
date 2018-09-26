d4855d7ebc9f400fc448f3d99ab27a2db218f9d9
Douglas Uretsky <douglasuretsky@gmail.com>
2018-09-26 18:26:55 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,10 +2,12 @@
 # out the current state.
 
 def display_board(board = Array.new(9) {:X})
-    |   |   
-print -----------
-    |   |   
------------
-    |   |  
+  def display_board
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end
 end
 
