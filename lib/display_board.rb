2a13c722d30056a0796e78e87f3d5fe0123f2bbf
Douglas Uretsky <douglasuretsky@gmail.com>
2018-09-26 18:31:37 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -4,7 +4,7 @@
 def display_board(board = Array.new(9) {:X})
   puts " #{board[0]}  | #{board[1]}  | #{board[2]}  "
   puts "---------------"
-  puts "  #{board[3]} |  #{board[4]} | #{board[5]}  "
+  puts " #{board[3]}  |  #{board[4]} | #{board[5]}  "
   puts "---------------"
   puts " #{board[6]}  | #{board[7]}  | #{board[6]}  "
 end
